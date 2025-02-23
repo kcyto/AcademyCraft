31c8fb5398ac2acff874b7004330f78c31b125c4
kcyto <kusuri@aliyun.com>
2025-02-23 21:50:03 +0800

    chore:code optimize BloodSpray and Smoke improvements
    Signed-off-by: kcyto <kusuri@aliyun.com>

--- src/main/scala/cn/academy/ability/vanilla/generic/client/effect/BloodSprayEffect.scala
+++ src/main/scala/cn/academy/ability/vanilla/generic/client/effect/BloodSprayEffect.scala
@@ -16,94 +16,100 @@ import net.minecraft.util.math.{BlockPos, Vec3d}
 import net.minecraft.world.World
 import net.minecraftforge.fml.client.registry.{IRenderFactory, RenderingRegistry}
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import org.lwjgl.opengl.GL11._
 
 @RegEntityRender(classOf[BloodSprayEffect])
 class BloodSprayRenderer(manager: RenderManager) extends Render[BloodSprayEffect](manager) {
-  import org.lwjgl.opengl.GL11._
+  private val TEXTURE_GROUND_PATHS = generateTexturePaths("grnd", 3)
+  private val TEXTURE_WALL_PATHS = generateTexturePaths("wall", 3)
 
-  val tex_grnd = seq("grnd", 3)
-  val tex_wall = seq("wall", 3)
+  private val mesh: LegacyMesh = {
+    val m = new LegacyMesh()
+    LegacyMeshUtils.createBillboard(m, -0.5, -0.5, 0.5, 0.5)
+    m
+  }
 
-  val mesh = new LegacyMesh
-  LegacyMeshUtils.createBillboard(mesh, -.5, -.5, .5, .5)
-  val material = new SimpleMaterial(null)
+  private val material = new SimpleMaterial(null)
 
-  private def seq(name: String, cnt: Int) = (0 until cnt)
-    .map(x => Resources.getTexture("effects/blood_spray/" + name + "/" + x))
-    .toVector
+  private def generateTexturePaths(prefix: String, count: Int): Vector[ResourceLocation] =
+    (0 until count).map(i => Resources.getTexture(s"effects/blood_spray/$prefix/$i")).toVector
 
-  override def doRender(eff: BloodSprayEffect, x: Double, y: Double, z: Double, f1: Float, f2: Float) = {
-    val list = if (eff.isWall) tex_wall else tex_grnd
-    val texture = list(eff.textureID % list.size)
+  override def doRender(
+                         eff: BloodSprayEffect,
+                         x: Double, y: Double, z: Double,
+                         partialTicks: Float,
+                         destroyProgress: Float
+                       ): Unit = {
+    val textures = if (eff.isWall) TEXTURE_WALL_PATHS else TEXTURE_GROUND_PATHS
+    val texture = textures(eff.textureID % textures.size)
 
     material.setTexture(texture)
-
     RenderUtils.loadTexture(texture)
-    glDisable(GL_CULL_FACE)
     glPushMatrix()
+    glDisable(GL_CULL_FACE)
 
     glTranslated(x, y, z)
-    glRotatef(-eff.rotationYaw,   0, 1, 0)
+    glRotatef(-eff.rotationYaw, 0, 1, 0)
     glRotatef(-eff.rotationPitch, 1, 0, 0)
     glTranslated(eff.planeOffset._1, eff.planeOffset._2, 0)
     glScaled(eff.size, eff.size, eff.size)
-    glRotated(eff.rotation,       0, 0, 1)
+    glRotated(eff.rotation, 0, 0, 1)
 
     mesh.draw(material)
 
-    glPopMatrix()
     glEnable(GL_CULL_FACE)
+    glPopMatrix()
   }
 
   override def getEntityTexture(entity: BloodSprayEffect): ResourceLocation = null
 }
 
 class BloodSprayEffect(world: World, pos: BlockPos, side: Int) extends LocalEntity(world) {
+  require(side >= 0 && side < EnumFacing.values().length, s"Invalid side value: $side")
 
-  val dir = EnumFacing.values()(side)
-  val textureID = RandUtils.rangei(0, 10)
+  private val dir = EnumFacing.values()(side)
+  val textureID: Int = RandUtils.rangei(0, 10)
 
-  val size = RandUtils.ranged(1.1, 1.4) * (if (side == 0 || side == 1) 1.0 else 0.8)
-  val rotation = RandUtils.ranged(0, 360)
-  val planeOffset = (rand.nextGaussian() * 0.15, rand.nextGaussian() * 0.15)
-
-  ignoreFrustumCheck = true
-
-  setSize(1.5f, 2.2f)
+  val size: Double = RandUtils.ranged(1.1, 1.4) * (if (dir.getAxis.isVertical) 1.0 else 0.8)
+  val rotation: Double = RandUtils.ranged(0, 360)
+  val planeOffset: (Double, Double) = (rand.nextGaussian() * 0.15, rand.nextGaussian() * 0.15)
 
   {
-    val blockState = world.getBlockState(pos)
-    val block = blockState.getBlock
-    def m(x: Double, y: Double) = (x + y) / 2
+    ignoreFrustumCheck = true
+    setSize(1.5f, 2.2f)
+    updatePosition()
+  }
 
+  private def updatePosition(): Unit = {
+    val blockState = world.getBlockState(pos)
     val bounds = blockState.getBoundingBox(world, pos)
 
-    val (dx, dy, dz) = (
-      bounds.maxX - bounds.minX,
-      bounds.maxY - bounds.minY,
-      bounds.maxZ - bounds.minZ
-      )
-    val (xm, ym, zm) = (m(bounds.minX, bounds.maxX),
-      m(bounds.minY, bounds.maxY),
-      m(bounds.minZ, bounds.maxZ))
+    val dx = bounds.maxX - bounds.minX
+    val dy = bounds.maxY - bounds.minY
+    val dz = bounds.maxZ - bounds.minZ
 
-    this.setPosition(
-      pos.getX + xm + dir.getXOffset * 0.51 * dx,
-      pos.getY + ym + dir.getYOffset * 0.51 * dy,
-      pos.getZ + zm + dir.getZOffset * 0.51 * dz
+    val cx = (bounds.minX + bounds.maxX) * 0.5
+    val cy = (bounds.minY + bounds.maxY) * 0.5
+    val cz = (bounds.minZ + bounds.maxZ) * 0.5
+
+    val offsetFactor = 0.51
+
+    setPosition(
+      pos.getX + cx + (dir.getXOffset * offsetFactor * dx).toDouble,
+      pos.getY + cy + (dir.getYOffset * offsetFactor * dy).toDouble,
+      pos.getZ + cz + (dir.getZOffset * offsetFactor * dz).toDouble
     )
   }
 
   new EntityLook(dir).applyToEntity(this)
 
-  override def onUpdate() = {
+  override def onUpdate(): Unit = {
     if (ticksExisted > 1200 || world.getBlockState(pos).getBlock == Blocks.AIR) {
       setDead()
     }
   }
 
-  override def shouldRenderInPass(pass: Int) = pass == 1
+  override def shouldRenderInPass(pass: Int): Boolean = pass == 1
 
-  def isWall = dir == EnumFacing.UP || dir == EnumFacing.DOWN
-
+  def isWall: Boolean = dir.getAxis.isVertical
 }
--- src/main/scala/cn/academy/ability/vanilla/generic/client/effect/SmokeEffect.scala
+++ src/main/scala/cn/academy/ability/vanilla/generic/client/effect/SmokeEffect.scala
@@ -4,101 +4,98 @@ import cn.academy.Resources
 import cn.academy.client.CameraPosition
 import cn.lambdalib2.render.legacy.Tessellator
 import cn.academy.entity.LocalEntity
-import cn.lambdalib2.util.{EntityLook, GameTimer, RenderUtils}
-import cn.lambdalib2.util.VecUtils._
-import net.minecraftforge.fml.client.registry.RenderingRegistry
-import net.minecraft.client.renderer.entity.{Render, RenderManager}
-import net.minecraft.entity.Entity
-import net.minecraft.util.ResourceLocation
-import net.minecraft.world.World
-import net.minecraftforge.fml.relauncher.{Side, SideOnly}
-import cn.lambdalib2.registry.StateEventCallback
 import cn.lambdalib2.registry.mc.RegEntityRender
+import cn.lambdalib2.util.{EntityLook, GameTimer, RenderUtils, VecUtils}
+import net.minecraftforge.fml.relauncher.{Side, SideOnly}
+import net.minecraft.client.renderer.entity.{Render, RenderManager}
+import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.Vec3d
-import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraft.world.World
 import org.lwjgl.opengl.GL11._
 
 @RegEntityRender(classOf[SmokeEffect])
 class SmokeEffectRenderer(m: RenderManager) extends Render[SmokeEffect](m) {
-  val texture = Resources.preloadTexture("effects/smokes")
+  private val TEXTURE = Resources.preloadTexture("effects/smokes")
+  private val FRAMES_PER_ROW = 2
+  private val FRAME_SIZE = 1.0 / FRAMES_PER_ROW
 
-  override def doRender(eff: SmokeEffect, x : Double, y : Double, z : Double, pt : Float, wtf : Float): Unit = {
-      val campos = CameraPosition.getVec3d
-      val delta = subtract(new Vec3d(x, y, z), campos)
-      val look = new EntityLook(delta)
+  override def doRender(
+                         eff: SmokeEffect,
+                         x: Double, y: Double, z: Double,
+                         partialTicks: Float,
+                         wtf: Float
+                       ): Unit = {
+    val campos = CameraPosition.getVec3d
+    val delta = VecUtils.subtract(new Vec3d(x, y, z), campos)
+    val look = new EntityLook(delta)
 
-      glEnable(GL_BLEND)
-      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-      glDisable(GL_ALPHA_TEST)
-      glDisable(GL_CULL_FACE)
-      glPushMatrix()
-      glTranslated(x, y, z)
+    glPushMatrix()
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glDisable(GL_ALPHA_TEST)
+    glDisable(GL_CULL_FACE)
 
-      glRotated(-look.yaw + 180, 0, 1, 0)//TODO
-      glRotated(-look.pitch, 1, 0, 0)
-      glScaled(eff.size, eff.size, 1)
+    glTranslated(x, y, z)
+    glRotated(-look.yaw + 180, 0, 1, 0)
+    glRotated(-look.pitch, 1, 0, 0)
+    glScaled(eff.size, eff.size, 1)
 
-      val (u, v) = ((eff.frame % 2) / 2.0, (eff.frame / 2) / 2.0)
+    val u = (eff.frame % FRAMES_PER_ROW) * FRAME_SIZE
+    val v = (eff.frame / FRAMES_PER_ROW) * FRAME_SIZE
 
-      val t = Tessellator.instance
-      glColor4f(1, 1, 1, eff.alpha)
-      RenderUtils.loadTexture(texture)
+    RenderUtils.loadTexture(TEXTURE)
+    glColor4f(1, 1, 1, eff.alpha)
 
-      t.startDrawingQuads()
-      t.addVertexWithUV(-1, -1, 0, u,     v    )
-      t.addVertexWithUV(-1,  1, 0, u,     v+0.5)
-      t.addVertexWithUV( 1,  1, 0, u+0.5, v+0.5)
-      t.addVertexWithUV( 1, -1, 0, u+0.5, v    )
-      t.draw()
+    val t = Tessellator.instance
+    t.startDrawingQuads()
+    t.addVertexWithUV(-1, -1, 0, u,          v)
+    t.addVertexWithUV(-1,  1, 0, u,          v + FRAME_SIZE)
+    t.addVertexWithUV( 1,  1, 0, u + FRAME_SIZE, v + FRAME_SIZE)
+    t.addVertexWithUV( 1, -1, 0, u + FRAME_SIZE, v)
+    t.draw()
 
-      glPopMatrix()
-      glEnable(GL_CULL_FACE)
-      glEnable(GL_ALPHA_TEST)
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_ALPHA_TEST)
+    glPopMatrix()
   }
 
   override def getEntityTexture(entity: SmokeEffect): ResourceLocation = null
 }
 
-/**
-  * @author WeAthFolD
-  */
 @SideOnly(Side.CLIENT)
 class SmokeEffect(world: World) extends LocalEntity(world) {
+  private val INIT_LIFE_TIME = 4.0f
+  private val FRAME_COUNT = 4
 
   setSize(1, 1)
 
-  val initTime = time
+  val initTime: Double = GameTimer.getTime
+  val frame: Int = rand.nextInt(FRAME_COUNT)
+  val lifeModifier: Float = 0.5f + rand.nextFloat() * 0.2f
+  val rotSpeed: Float = 0.3f * (rand.nextFloat() + 3)
 
-  var rotation = 0.0f
-  var size = 1.0f
+  var size: Float = 1.0f
+  var rotation: Float = 0.0f
 
-  private val lifeModifier = 0.5f + rand.nextFloat() * 0.2f
-
-  private val rotSpeed = 0.3f * (rand.nextFloat() + 3)
-
-  private[effect] val frame = rand.nextInt(4)
-
-  override def onUpdate() = {
+  override def onUpdate(): Unit = {
     rotation += rotSpeed
-
     posX += motionX
     posY += motionY
     posZ += motionZ
 
-    if (deltaTime >= 4f) { setDead() }
+    if (deltaTime >= INIT_LIFE_TIME) setDead()
   }
 
-  private[effect] def alpha: Float = 1.0f * ((deltaTime / lifeModifier).toFloat match {
-    case dt if dt <= 0.3f => dt / 0.3f
-    case dt if dt <= 1.5f => 1.0f
-    case dt if dt <= 2 => 1 - (dt - 1.5f) / 0.5f
-    case _ =>  0.0f
-  })
+  def alpha: Float = {
+    val progress = (deltaTime / lifeModifier).toFloat
+    progress match {
+      case p if p <= 0.3f => p / 0.3f
+      case p if p <= 1.5f => 1.0f
+      case p if p <= 2.0f => 1 - (p - 1.5f)/0.5f
+      case _ => 0.0f
+    }
+  }
 
-  private def deltaTime = (time - initTime)
-
-  private def time = GameTimer.getTime
-
-  override def shouldRenderInPass(pass: Int) = pass == 1
-
+  private def deltaTime: Double = GameTimer.getTime - initTime
+  override def shouldRenderInPass(pass: Int): Boolean = pass == 1
 }
